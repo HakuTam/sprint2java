525aa592cf1edadc15e084130527a238dd96438e
Cedilhasemc <contact.toyku@gmail.com>
2023-05-21 22:51:57 -0300

    corrigindo bugs, botão no home desconfigurado

---
+++ sprint2java/script.js
@@ -0,0 +1,11 @@
+document.getElementById("botao-calculadora").onclick = function () {
+    location.href = "http://127.0.0.1:5500/sprint2java/pages/calculadora.html";
+}
+
+document.getElementById("botao-sobrenos").onclick = function () {
+    location.href = "http://127.0.0.1:5500/sprint2java/pages/sobrenos.html";
+}
+
+document.getElementById("botao-contato").onclick = function () {
+    location.href = "http://127.0.0.1:5500/sprint2java/pages/contato.html";
+}
